852eff3bb1c6a9d9fbb191007a1416a7c9b16b60
Bryce <brycemwilson@gmail.com>
2015-12-01 20:31:21 -0600

    Add tile and bounce scripts

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce : MonoBehaviour {
+	public float amplitude;
+	public float forwardVel;
+	public float bounceAngle;
+
+	public AnimationCurve bounceCurve;
+
+	protected Vector2 bounceNormal;
+	protected Vector2 bounceFloor;
+	protected float height;
+
+	// Use this for initialization
+	void Start () {
+		bounceFloor = new Vector2 (transform.position.x, transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos;
+
+		// update height
+		height = bounceCurve.Evaluate (Time.time);
+
+		// update position
+		bounceNormal = new Vector2 (Mathf.Cos (bounceAngle), Mathf.Sin (bounceAngle));
+		pos.x = transform.position.x + bounceFloor.x + (bounceNormal.x * height);
+		pos.y = transform.position.y + bounceFloor.y + (bounceNormal.y * height);
+		pos.z = transform.position.z + (forwardVel * Time.deltaTime);
+		transform.position = pos;
+	}
+	
+	public void Bounce(float bounceFac) {
+		height = amplitude * bounceFac;
+	}
+}
---
+++ Assets/Scripts/TunnelTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TunnelTile : MonoBehaviour {
+	public float bounceFactor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+		foreach (ContactPoint c in col.contacts) {
+			GameObject go = c.otherCollider.gameObject;
+			if(go.tag.Equals("bouncer")) {
+				Bounce bouncer = go.GetComponent<Bounce>();
+				if(bouncer != null) {
+					bouncer.Bounce(bounceFactor);
+				}
+			}
+		}
+	}
+}
