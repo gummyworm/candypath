bc50b54effd3d8081b46557e451b91b1c847d9df
Bryce <brycemwilson@gmail.com>
2015-12-01 21:36:37 -0600

    Add Generate to TunnelGen

--- Assets/Scripts/TunnelGen.cs
+++ Assets/Scripts/TunnelGen.cs
@@ -4,15 +4,20 @@ using System.Collections.Generic;
 
 public class TunnelGen : MonoBehaviour {
 	public int tilesPerStep;
+	public float stepSize;
+	public float diameter;
 
+	public GameObject[] tunnel;
 
 	protected Dictionary<int, TunnelTile> tileMap;
 	protected List<TunnelTile[]> tiles; // tiles is the array of tunnel segments
+	protected float angleStep; // radians between each tile in a segment
 
 	// Use this for initialization
 	void Start () {
 		tileMap = new Dictionary<int, TunnelTile> ();
 		tiles = new List<TunnelTile[]> ();
+		angleStep = (Mathf.PI*2.0f)/tilesPerStep;
 	}
 	
 	// Update is called once per frame
@@ -26,7 +31,7 @@ public class TunnelGen : MonoBehaviour {
 	}
 
 	// AddSegment adds a segment of tiles from the given array of tile ID's.
-	public void AddSegment(int []tiles) {
+	public void AddSegment(int[] tiles) {
 		TunnelTile segment = new TunnelTile[tilesPerStep];
 		for(int i = 0; i < tiles.Length; ++i) {
 			if(tileMap.ContainsKey(tiles[i])) {
@@ -35,6 +40,22 @@ public class TunnelGen : MonoBehaviour {
 			else {
 				segment[i] = null;
 			}
+		}
+	}
+
+	// Generates a tunnel from all the segments that have been added.
+	public void Generate() {
+		Vector3 spawnAt = Vector3.zero;
+
+		foreach (TunnelTile[] segment in tiles) {
+			int i;
+			float theta;
+			for(int i = 0, theta = 0.0f; i < segment.Length; ++i, theta += angleStep) {
+				spawnAt.x += Mathf.Cos(theta) * diameter;
+				spawnAt.y += Mathf.Sin(theta) * diameter;
+				GameObject.Instantiate(segment[i], spawnAt, Quaternion.identity);
+			}
+			spawnAt.z += stepSize;
 		}
 	}
 }
